9501050d32e6690715d4e96fdd2225f34febd769
jparra <jparra@sdgku.edu>
2025-10-31 08:07:22 -0700

    feat: Actualización de estilos y componentes en la tienda online

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,34 @@
- import './Navbar.css';
+import './Navbar.css';
 
 function Navbar() {
     return (
-        <nav className="navbar">
-            <div className="navbar-container">
-                <div className="navbar-brand">
-                    <h1>My Online Store</h1>
-                </div>
-                <div className="navbar-menu">
+
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">My Online Store</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a href="#home" className="nav-link">Home </a>
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
-
                         <li className="nav-item">
-                            <a href="#catalog" className="nav-link">Catalog</a>
+                            <a className="nav-link" href="#">Catalog</a>
                         </li>
-
                         <li className="nav-item">
-                            <a href="#about" className="nav-link">About</a>
+                            <a className="nav-link" href="#">About</a>
                         </li>
-                    </ul >
-                </div >
-            </div >
-        </nav >
+                        <li className="nav-item">
+                            <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
     );
 }
 export default Navbar;
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -4,29 +4,64 @@ import DataService from '../services/dataService'
 import { useEffect, useState } from 'react'
 
 function Catalog() {
-  
-  const [products, setProducts] = useState([]) 
-  
+
+  const [products, setProducts] = useState([])
+  const [categories, setCategories] = useState([]) // to hold the category list
+  const [prodsToDisplay, setProdsToDisplay] = useState([])//to hold the products to show
+
   useEffect(() => {
     //Load all our data
     console.log("Component loaded")
-    loadCatalog()  
-  },[]
-) //The empty array [] menas this effect runs only ONCE
+    loadCatalog()
+  }, []
+  ) //The empty array [] menas this effect runs only ONCE
 
-function loadCatalog(){
+  function loadCatalog() {
     let service = new DataService()
     let data = service.getProducts()
     setProducts(data)
-}
+    setProdsToDisplay(data) // Initially display all products
+    //TODO: Move this to a service later
+    let cats = ["fruit", "Vegetables", "Meat", "Dairy & Eggs", "Beverages", "Wine & Beer"]
+    setCategories(cats)
+  }
+
+  function clearFilter() {
+    setProdsToDisplay(products)
+  }
+
+  function filter(category) {
+    let list = []
+    //loop through products and add the ones that match the category to the list
+    products.forEach(prod => {
+      if (prod.category === category) {
+        list.push(prod)
+      }
+    })
+    setProdsToDisplay(list)
+  }
+
+  
 
   return (
     <div className="catalog">
       <h1>Check our amazing products</h1>
-      <div className="product-list">
-        {products.map(prod => <Product key={prod._id} tax={"taxes"} refound={"value here"} data={prod}/>)}
+
+
+      <div className="filters">
+        <button onClick={clearFilter} className="btn btn-dark btn-filter">
+          All Products
+        </button>
+        {categories.map(
+          cat => <button key={cat} onClick={() => filter(cat)} className="btn btn-primary btn-filter">{cat}
+          </button>
+        )}
       </div>
-     
+
+      <div className="product-list">
+        {prodsToDisplay.map(prod => <Product key={prod._id} data={prod} />)}
+      </div>
+
     </div>
   )
 }
